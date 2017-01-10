e13ff577934924fd718ff0903521f2699f32441f
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2017-01-10 10:50:26 -0200

    Cria model consulta

--- servicos/models.py
+++ servicos/models.py
@@ -1,2 +1,12 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 from usuarios.models import Usuario
+
+
+class Consulta(models.Model):
+    medico = models.ForeignKey(
+        Usuario, verbose_name=_('Médico'), related_name='medico')
+    paciente = models.ForeignKey(
+        Usuario, verbose_name=_('Paciente'), related_name='paciente')
+    descricao = models.TextField(verbose_name=_('Descrição'))
