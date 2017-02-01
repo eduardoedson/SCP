7d4bdbd3c4b00758f9fd14dcd11b14b73a66273d
Eduardo Edson Batista Cordeiro Alves <eduardoalves@interlegis.leg.br>
2017-02-01 11:46:35 -0200

    Models para chamado

--- servicos/models.py
+++ servicos/models.py
@@ -23,3 +23,28 @@ class Consulta(models.Model):
 
     def __str__(self):
         return '-'
+
+
+class StatusChamado(models.Model):
+    descricao = models.CharField(
+        max_length=30, verbose_name=('Descrição'), unique=True)
+
+    class Meta:
+        verbose_name = _('Tipo de Usuário')
+        verbose_name_plural = _('Tipos de Usuários')
+
+    def __str__(self):
+        return self.descricao
+
+
+class Chamado(models.Model):
+    titulo = models.CharField(max_length=30, verbose_name=('Título'))
+    descricao = models.CharField(max_length=30, verbose_name=('Descrição'))
+    status = models.ForeignKey(StatusChamado, verbose_name=_('Status'))
+
+    class Meta:
+        verbose_name = _('Tipo de Usuário')
+        verbose_name_plural = _('Tipos de Usuários')
+
+    def __str__(self):
+        return '%s [%s]' % (self.titulo, self.status)
